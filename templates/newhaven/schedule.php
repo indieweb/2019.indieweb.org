82d19f3075f3847f94fb03b633c41a7058d86703
Greg McVerry <jgregmcverry@gmail.com>
2018-12-03 06:48:31 -0500

    Add files via upload
    added new haven

---
+++ templates/newhaven/schedule.php
@@ -0,0 +1,97 @@
+<p>
+  <i class="calendar icon"></i> <time class="dt-start" datetime="2018-03-30">March 30</time>-<time class="dt-end" datetime="2018-03-31">31</time>, 2019<br>
+  <i class="marker icon"></i> <span class="h-card p-location"><span class="p-name">Davis Hall, Southern Coneccticut State University</span>, <span class="p-street-address">501 Crescent Street</span>, <span class="p-locality">New Haven</span>, <span class="p-region">CT</span></span>
+</p>
+
+<div class="schedule-grid">
+<!--  <div class="day">-->
+<!--    <div class="header gold-bkg">-->
+<!--        Friday, February 22nd<br/>-->
+<!--        <span class="featured">Pre-Camp Meetup</span>-->
+<!--    </div>-->
+<!--    <div class="summary">-->
+<!--        The Pre-Camp Meetup is an informal social meetup for IndieWebCamp participants to catch up and get to know each other before the camp. -->
+<!--    </div>-->
+<!--    <div class="details">-->
+<!--        <ul>-->
+<!--            <li><span class="time">19:00</span> <span class="featured">Pre-Camp Meetup!</span></li>-->
+<!--        </ul>-->
+<!--        <span><a href="http://joesquared.com/">Joe Squared</a><br/><span>33 W. North Ave.</span></span>-->
+<!--        <p style="font-size:0.8em; margin-top: 1em;">Joe Squared is in Baltimore's Station North with great pizza, risotto, drink options, and more. Vegan and GF friendly.</p>-->
+<!--        <ul>-->
+<!--            <li><span class="time">21:00</span> <span class="featured">IndieWeb-themed* Comedy Show</span></li>-->
+<!--        </ul>-->
+<!--        <span><a href="http://www.bigimprov.org/">Baltimore Improv Group</a><br/><span>1727 N. Charles St.</span></span>-->
+<!--        <p style="font-size:0.8em; margin-top: 1em;">The Baltimore Improv Group (BIG) is a non-profit for improvisational theatre shows and instruction. Co-organizer Marty will be appearing in the evening's 9PM show.</p>-->
+<!--    </div>-->
+<!--  </div>-->
+  <div class="day">
+    <div class="header orange-bkg">
+      Saturday, March 30th<br>
+      <span class="featured">Learn, Share, Discuss!</span>
+    </div>
+    <div class="summary">
+      Day 1 starts with introductions, demos of personal sites, a review of the IndieWeb community and development building blocks, then BarCamp-style brainstorming and discussion sessions.
+    </div>
+    <div class="details">
+      <ul>
+          <li>TBD</li>
+<!--        <li><span class="time">9:00</span> Doors open</li>-->
+<!--        <li><span class="time">10:00</span> <a href="https://indieweb.org/2018/Baltimore/Introductions">Introductions &amp; Personal Site Demos</a></li>-->
+<!--        <li><span class="time">10:45</span> IndieWeb Building Blocks</li>-->
+<!--        <li><span class="time">11:30</span> <a href="https://indieweb.org/session_facilitating">Session Proposals &amp; Scheduling</a></li>-->
+<!--        <li><span class="time">12:00</span> Group photo &amp; Lunch</li>-->
+<!--        <li><span class="time">13:15 - 17:40</span> <a href="https://indieweb.org/2018/Baltimore/Sessions">Sessions</a> (45min)</li>-->
+<!--        <li><span class="time">17:45 - 18:00</span> Intro to Day 2</li>-->
+<!--        <li>Self-Organized Dinner</li>-->
+      </ul>
+    </div>    
+  </div>
+  <div class="day">
+    <div class="header red-bkg">
+      Sunday, March 31st<br>
+      <span class="featured">Create, Hack, Demo!</span>
+    </div>
+    <div class="summary">
+      Day 2 make things on and for your personal site! Collaborate with others or work on your own to improve at least one thing on your personal site, no matter how small or how epic.
+    </div>
+    <div class="details">
+      <ul>
+          <li>TBD</li>
+<!--        <li><span class="time">9:00</span> Doors open, badges, coffee, and breakfast!</li>-->
+<!--        <li><span class="time">9:30</span> Day 2 kickoff &amp; project intros</li>-->
+<!--        <li><span class="time">10:00</span> Start creating!</li>-->
+<!--        <li><span class="time">12:00</span> Group photo &amp; lunch</li>-->
+<!--        <li><span class="time">13:00</span> Creating sessions continue</li>-->
+<!--        <li><span class="time">16:00</span> <a href="https://indieweb.org/2018/Baltimore/Demos">Demos</a></li>-->
+<!--        <li><span class="time">17:15</span> <span class="featured">Camp closed!</span></li>-->
+      </ul>
+    </div>
+  </div>
+</div>
+
+<br>
+
+<!--
+
+45-minute sessions + 10 minute break
+
+13:15 - 14:00
+14:10 - 14:55
+15:05 - 15:50
+16:00 - 16:45
+16:55 - 17:40
+17:45 - 18:00 Intro to Day 2
+
+
+45-minute sessions + 10 minute break
+
+13:30 - 14:15
+14:25 - 15:10
+15:25 - 16:05
+16:15 - 17:00
+17:10 - 17:55
+17:55 - 18:00 Intro to Day 2
+
+-->
+
