82d3400a3ab9dc9ecbcbeb4357078b31c9155986
Eddie Hinkle <eddie@eddiehinkle.com>
2019-02-05 11:05:41 -0500

    Add IWC Online 2019

---
+++ templates/online/sponsors.php
@@ -0,0 +1,16 @@
+<!--<p>Special thanks to our sponsors who make it possible to provide food and space for the IndieWebCamp</p>-->
+
+<ul>
+<!--  <li>
+    <span class="logo">
+      <a href="https://www.digitalharbor.org/"><img src="https://indieweb.org/sponsor-logos/full/digital-harbor-foundation-logo.png" style="width: 130px;"></a>
+    </span>
+    <span class="description"><a href="https://www.digitalharbor.org/">The Digital Harbor Foundation</a> is dedicated to fostering learning, creativity, productivity, and community through education. In 2013 we transformed a closed-down rec center in Baltimore City into a vibrant Tech Center for youth.</span>
+  </li>-->
+</ul>
+
+
+<h4 class="ui header">How to Sponsor</h4>
+<p>We would be thrilled to list you as a sponsor! See our sponsorship prospectus for details, or <a href="https://opencollective.com/indieweb">become a monthly supporter</a>.</p>
+<a class="ui button" href="https://indiewebcamp.com/images/2/2d/indiewebcamp-sponsorship-prospectus.pdf">Download Sponsorship Prospectus (PDF)</a>
+
