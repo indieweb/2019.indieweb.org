7ac6c899fe4dd0259e3bf9f9bcc4ffcd8dd09d32
Marty McGuire <schmartissimo@gmail.com>
2019-02-27 13:12:46 -0500

    ti.to event baltimore-2018 => new-haven

--- public/newhaven.php
+++ public/newhaven.php
@@ -112,7 +112,7 @@ include(dirname(__FILE__).'/../lib/rsvps.php');
   <div class="ui vertical stripe segment" id="register">
     <div class="ui text container">
       <h3 class="ui header">Registration TBA</h3>
-     <tito-widget event="indiewebcamp/baltimore-2018"><a href="https://ti.to/indiewebcamp/baltimore-2018">Get Tickets</a></tito-widget>
+     <tito-widget event="indiewebcamp/new-haven"><a href="https://ti.to/indiewebcamp/new-haven">Get Tickets</a></tito-widget>
 
     </div>
   </div>
