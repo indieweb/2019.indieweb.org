896b4c9e70bb7e50e636e37f5a84af03cc2bf82c
Aaron Parecki <aaron@parecki.com>
2019-06-13 22:28:55 -0700

    travel assistance is closed

--- templates/summit/assistance.php
+++ templates/summit/assistance.php
@@ -1,3 +1,7 @@
+<div style="border: 1px ##f4b058 solid; background: #ffd9a8; border-radius: 8px; padding: 12px; margin-bottom: 2em;">
+	Travel assistance applications are now closed, and all applicants have been notified. Thanks to everyone who submitted an application! Please see our <a href="https://indieweb.org/2019#Remote_Participation">remote particiation</a> options if you are not able to attend in person.
+</div>
+
 <p>The IndieWeb community is building the foundation of a web controlled by individuals for themselves. It's crucial to have independent web creators from all backgrounds at IndieWeb Summit to have a rich discussion and build cool things.</p>
 
 <p>If you are a member of a group that is typically underrepresented (e.g. if you are not hetero, white, cis and male), and otherwise could not afford to travel to IndieWeb Summit on your own, generous donations from sponsors and other IndieWeb Summit attendees are able to be put towards a fund to assist individuals from underrepresented backgrounds with travel and/or lodging costs for the Indieweb Summit in Portland.</p>
