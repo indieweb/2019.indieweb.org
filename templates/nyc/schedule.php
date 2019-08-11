360ac89b1fc912a9e7423c5656ed73a53412ab61
Marty McGuire <schmartissimo@gmail.com>
2019-08-11 15:55:11 -0400

    first pass at IWC NYC 2019

---
+++ templates/nyc/schedule.php
@@ -0,0 +1,157 @@
+<p>
+  <i class="calendar icon"></i> <time class="dt-start" datetime="2019-10-05">October 5</time>-<time class="dt-end" datetime="2019-10-06">6</time>, 2019<br>
+  <i class="marker icon"></i> <span class="h-card p-location"><span class="p-name">Pace University</span>, <span class="p-street-address">163 William St 2nd Floor</span>, <span class="p-locality">New York City</span>, <span class="p-region">New York</span></span>
+</p>
+
+<div class="schedule-grid">
+  <!--
+  <div class="day h-event">
+    <div class="header gold-bkg">
+      <time class="dt-start" datetime="2019-06-28T17:30:00-0700">Friday, June 28</time><br>
+      <span class="featured p-name">Pre-Summit Meetup</span>
+    </div>
+    <div class="summary p-description">
+      The Pre-Summit Meetup is an informal social meetup for IndieWeb Summit participants to catch up and get to know each other before the summit.
+    </div>
+    <div class="details">
+      <ul>
+        <li><span class="time">17:30</span> <span class="featured">Pre-Summit Meetup!</span></li>
+      </ul>
+      <span class="p-location h-card"><a class="p-name u-url" href="http://www.pinestreetpdx.com/">Pine Street Market</a><br><span class="p-street-address">126 SW 2nd St.</span></span>
+      <p style="font-size:0.8em; margin-top: 1em;">Pine Street Market is an indoor food hall with a wide variety of food and drink options.</p>
+    </div>
+  </div>
+  -->
+  <div class="day">
+    <div class="header orange-bkg">
+      Saturday, October 5<br>
+      <span class="featured">Keynotes &amp; Discussions</span>
+    </div>
+    <div class="summary">
+      Day 1 starts with keynotes and demos, then BarCamp-style brainstorming and discussion sessions.
+    </div>
+    <div class="details">
+      <ul>
+        <li><span class="time">09:00</span> Doors open, badges, coffee, and breakfast!</li>
+        <li><span class="time">10:00</span> <span class="featured"><a href="https://indieweb.org/2019/NYC/Schedule#Keynotes">Keynotes</a></span></li>
+        <li><span class="time">11:00</span> Lightning Intros &amp; Personal Site Demos</li>
+        <li><span class="time">12:00</span> Group photo &amp; decentralized lunch</li>
+        <li><span class="time">13:30</span> Session Proposals &amp; Scheduling</li>
+        <li><span class="time">14:00</span> Sessions (45 minute slots)</li>
+        <li><span class="time">17:30 - 17:45</span> Day 1 closing session</li>
+      </ul>
+    </div>
+  </div>
+  <div class="day">
+    <div class="header red-bkg">
+      Sunday, October 6<br>
+      <span class="featured">Create, Hack, Demo!</span>
+    </div>
+    <div class="summary">
+      Day 2 is for making things on and for your personal site! Collaborate with others or work on your own to improve at least one thing on your personal site, no matter how small or how epic.
+    </div>
+    <div class="details">
+      <ul>
+        <li><span class="time">09:00</span> Doors open, badges, coffee, and breakfast!</li>
+        <li><span class="time">09:30</span> Day 2 session scheduling</li>
+        <li><span class="time">10:00</span> <span class="featured">IndieWeb Creating:</span> <br>IndieWeb 101, IndieWebifying your WordPress, Dev intro to indieweb building blocks</li>
+        <li><span class="time">11:00</span> Start creating!</li>
+        <li><span class="time">12:00</span> Group photo &amp; catered lunch</li>
+        <li><span class="time">13:00</span> Creating sessions continue</li>
+        <li><span class="time">16:00</span> Demos</li>
+        <li><span class="time">17:30</span> <span class="featured">Closing Keynote</span></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<br>
+<p>Schedule is subject to change. View the <a href="https://indieweb.org/2019/NYC/Schedule">full schedule grid</a> on the IndieWeb wiki for the latest updates.</p>
+
+
+<!--
+<h3 class="ui header">Keynotes</h3>
+
+<div class="keynote-speakers">
+
+  <div class="speaker">
+    <a href="https://tantek.com/"><img src="/images/tantek.jpg"></a>
+    <p class="name">Tantek<br> Çelik</p>
+    <p class="role">State of the IndieWeb</p>
+  </div>
+
+  <div class="speaker">
+    <a href="https://kitt.hodsden.org/"><img src="/images/kitt-hodsden.jpg"></a>
+    <p class="name">Kitt<br> Hodsden</p>
+    <p class="role">On Contractions &amp; Expansions</p>
+  </div>
+
+  <div class="speaker">
+    <a href="https://mjordan.codes/"><img src="/images/mjordan.jpg"></a>
+    <p class="name">mJordan<br>&nbsp;</p>
+    <p class="role">Changing My Domain</p>
+  </div>
+
+  <div class="speaker">
+    <a href="https://martymcgui.re/"><img src="/images/schmarty.jpg"></a>
+    <p class="name">Marty<br> McGuire</p>
+    <p class="role">Own Your Mobile Experience</p>
+  </div>
+
+  <div class="speaker">
+    <a href="https://jacky.wtf/"><img src="/images/jacky.jpg"></a>
+    <p class="name">Jacky<br> Alciné</p>
+    <p class="role">Making the IndieWeb for All</p>
+  </div>
+
+</div>
+-->
+
+
+<!--
+<br>
+
+<h3>Related Events</h3>
+
+<p>There's a lot happening the week of IndieWeb Summit! Here are some related events. These events have their own registration process and community guidelines so please register on their respective websites!</p>
+
+<div class="related-event">
+  <h4>Tuesday, June 25: Donut.js</h4>
+  <p><b><a href="https://donutjs.club/">Donut.js</a></b> is a monthly meet-up in Portland, OR with a handful of wonderful talks on tech, creativity, community, and the places they come together.</p>
+  <p><a href="https://donutjs.club/" class="register-btn">Register Here</a> ($10 or free)</p>
+</div>
+
+<div class="related-event">
+  <h4>Wednesday, June 26: Homebrew Website Club</h4>
+  <p><b><a href="https://indieweb.org/events/2019-06-26-homebrew-website-club#Portland">Homebrew Website Club</a></b> is a meetup of people passionate about or interested in creating, improving, building, designing their own website.</p>
+  <p><a href="https://indieweb.org/events/2019-06-26-homebrew-website-club#Portland" class="register-btn">RSVP Here</a> (free, or just show up!)</p>
+</div>
+
+<div class="related-event">
+  <h4>Thursday, June 27: Hack the Dot</h4>
+  <p><b><a href="https://www.eventbrite.com/e/hack-the-dot-alchemy-code-lab-tickets-62233446915">Hack the Dot</a></b> is a two-hour hackathon where coding school students, junior developers, experienced developers, non developers, marketers, designers, and those with little or no computer programming skill come together to build ideas around a single domain name.</p>
+  <p><a href="https://www.eventbrite.com/e/hack-the-dot-alchemy-code-lab-tickets-62233446915" class="register-btn">Register Here</a> (free)</p>
+</div>
+
+<div class="related-event">
+  <h4>Saturday, June 29: Dat Night</h4>
+  <p><b><a href="https://datnight.org/">Dat Night</a></b> is for people excited
+  about the future of the internet, whether it's decentralized,
+  distributed, peer to peer, federated, or all of the above. Come out
+  for snacks, drinks, and friends!</p>
+  <p><a href="https://datnight.org/" class="register-btn">Register Here</a> ($5 or free)</p>
+</div>
+
+-->
+
+<style>
+.related-event {
+  margin-top: 3em;
+}
+.register-btn {
+  background: #4183c4;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 8px;
+}
+</style>
