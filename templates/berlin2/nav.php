04e20fa4ce3b140f3a381bbe2b433c98858f6659
Rosemary Orchard <me@rosemaryorchard.com>
2019-10-13 07:41:41 +0200

    Add Berlin2

---
+++ templates/berlin2/nav.php
@@ -0,0 +1,5 @@
+    <a class="active item" href="#about">About</a>
+    <a class="item" href="#register">Register</a>
+    <a class="item" href="#schedule">Schedule</a>
+    <a class="item" href="#location">Location</a>
+    <a class="item" href="#sponsors">Sponsors</a>
