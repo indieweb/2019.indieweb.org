2584b9661e8f17478183aa87c5301a097dcb003f
Tantek Çelik <blog@tantek.com>
2020-03-06 16:40:59 -0800

    comment out Register

--- public/online.php
+++ public/online.php
@@ -110,7 +110,7 @@ include(dirname(__FILE__).'/../lib/rsvps.php');
 
   </div>
 
-
+<!--
   <div class="ui vertical stripe segment" id="register">
     <div class="ui text container">
       <h3 class="ui header">Register</h3>
@@ -118,7 +118,7 @@ include(dirname(__FILE__).'/../lib/rsvps.php');
 
     </div>
   </div>
-
+-->
 
   <div class="ui vertical stripe segment" id="rsvps">
     <div class="ui text container">
